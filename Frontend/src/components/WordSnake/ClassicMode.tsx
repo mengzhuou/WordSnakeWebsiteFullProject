c16c606db83ec0bfe5a527e3f7f35c50898dca46
mengzhuou <71995520+mengzhuou@users.noreply.github.com>
2023-02-17 12:21:35 -0500

    Added css to classicmode

--- Frontend/src/components/WordSnake/ClassicMode.tsx
+++ Frontend/src/components/WordSnake/ClassicMode.tsx
@@ -1,41 +1,61 @@
-import React, { Component } from 'react';
+import "./ClassicMode.css";
+import { withFuncProps } from "../withFuncProps";
+import {logout, getWordAndDefTest} from '../../helpers/connector';
+import { TextField } from "@mui/material";
+import React from "react";
 
-interface MyComponentProps {
-  // add any props here
+interface MyComponentState{
+    word: string;
 }
 
-interface MyComponentState {
-  // add any state properties here
+class ClassicMode extends React.Component<any,any>{
+    constructor(props:any){
+        super(props);
+        this.state = {word:"", ForceUpdateNow: false};
+        this.forceup = this.forceup.bind(this);
+        this.menuNav = this.menuNav.bind(this);
+    }
+
+    menuNav = () => {
+        this.props.navigate("/menu")
+    }
+
+    pagelogout = ()=>{
+        logout().then(()=>{
+            this.props.navigate("/")
+        }).catch(()=>(alert("logout error")));
+    }
+    forceup(){
+    }
+
+    componentDidMount(): void {
+        this.forceup();
+    }
+
+    componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<any>, snapshot?: any): void {
+        if(this.state.ForceUpdateNow){
+            this.forceup();
+        }
+    }
+    
+    render(){
+        return (
+            <div className="App">
+                <div className="topnav">
+                    <button className="topnavButton" onClick={this.menuNav}>Menu</button>
+                    <button className="topnavButton" onClick={this.pagelogout}>Logout</button>
+                </div>    
+                <h1 className="wsTitle">Word Snake</h1>
+                <div>
+                    <TextField
+                        label="Word start with "
+                        
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
-class ClassicMode extends Component<MyComponentProps, MyComponentState> {
-  constructor(props: MyComponentProps) {
-    super(props);
 
-    this.state = {
-      // initialize state here
-    };
-  }
-
-  componentDidMount() {
-    // code to run after the component mounts
-  }
-
-  componentDidUpdate(prevProps: MyComponentProps, prevState: MyComponentState) {
-    // code to run after the component updates
-  }
-
-  componentWillUnmount() {
-    // code to run before the component unmounts
-  }
-
-  render() {
-    return (
-      <div>
-        // code to render the component goes here
-      </div>
-    );
-  }
-}
-
-export default ClassicMode;
+export default withFuncProps(ClassicMode);
